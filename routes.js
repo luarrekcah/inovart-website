84ac3ca1dc169bba39b1fd98edc2cf14f281b023
Glitch (inovart) <none>
2021-12-03 20:57:36 +0000

    🙋👢 Checkpoint ./package.json:21151839/3816 ./server.js:21151839/1506 ./routes.js:21151839/8674 ./public/views/index.ejs:21151839/6564 ./public/views/partials/header.ejs:21151839/4522 ./public/views/partials/footer.ejs:21151839/1951

---
+++ routes.js
@@ -0,0 +1,18 @@
+module.exports = function(app) {
+  app.get("/ping", (req, res) => {
+    res.sendStatus(200);
+    const ping = new Date();
+    ping.setHours(ping.getHours() - 3);
+    console.log(
+      `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
+    );
+  });
+
+  app.get("/", function(req, res) {
+    res.render("index");
+  });
+
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
